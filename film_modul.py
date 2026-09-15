9d8d83107ee64c44efb6c0d06af1c9e4a21ca9e0
Freddie9813 <larsson9813@hotmail.com>
2026-09-15 13:34:34 +0200

    Add files via upload

--- film_modul.py
+++ film_modul.py
@@ -3,12 +3,14 @@ class Film:
         self.titel = titel
 
 
-
 def wrong_input():
     print("\nFel: Du måste välja ett alternativ från listan (1-5).")
+    
 
 def film_not_found(film_search):
     print(f"\n{film_search} finns inte i registret!")
 
+
 def film_found(film_search):
     print(f"\n{film_search} finns i registret!")
+
