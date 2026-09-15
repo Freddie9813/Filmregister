082e907eab775a4727ffe684f9c22c69be489b69
Freddie9813 <larsson9813@hotmail.com>
2026-09-15 13:31:00 +0200

    Add files via upload

--- Filmregister.py
+++ Filmregister.py
@@ -9,148 +9,107 @@ film_register = [film_modul.Film("Inception"),
 
 
 while True:
-    
-
     print("\n---Filmregister---")
-
     print("1. Visa filmregister")
     print("2. Lägg till film i register: ")
     print("3. Sök efter film: ")
     print("4. Ta bort film: ")
     print("5. Avsluta")
 
-
     
     try:
-        meny_val = int(input("Välj ett alternativ (1-5): "))
-        
-
+        meny_val = int(input("Välj ett alternativ (1-5): "))     
     except ValueError:
-        
         film_modul.wrong_input()
-        
         continue
 
 
-##------Menyval 1------------------------------------------------------------------------------------------------------------------------------------
+    # Menyval 1: Visa lista
     if meny_val == 1:
-
-        print("\n Filmregister:" )
-        
-        for film in film_register:
-            
+        print("\n Filmregister:")        
+        for film in film_register:            
             print("\n", film.titel)
 
-
-##------Menyval 2------------------------------------------------------------------------------------------------------------------------------------
+            
+    # Menyval: 2 Lägg till
     elif meny_val == 2:
-
         while True:
-            film_add = input("\nAnge film att lägga till i registret eller tryck 'Enter' för att återgå till startmenyn: ")
+            film_add = input(
+                "\nAnge film att lägga till i registret eller "
+                "tryck 'Enter' för att återgå till startmenyn: ")
+
 
             if film_add == "":
-
                 print("\nÅtergår till startmeny")
-
                 break
 
 
-            elif film_add:
-                
-                ny_film = film_modul.Film(film_add)
-                
-                film_register.append(ny_film)
-                
-                print("\nDu har valt att lägga till : " + film_add)
-                
+            elif film_add:                
+                ny_film = film_modul.Film(film_add)                
+                film_register.append(ny_film)                
+                print(f"\nDu har valt att lägga till: {film_add}")                
                 break
 
         
-##------Menyval 3------------------------------------------------------------------------------------------------------------------------------------
+    # Menyval 3: Sök
     elif meny_val == 3:
-        while True:
-            
-            film_search = input("\nAnge vilken film du söker efter eller tryck 'Enter' för att återgå till startmenyn: ") 
+        while True:            
+            film_search = input(
+                "\nAnge vilken film du söker efter eller "
+                "tryck 'Enter' för att återgå till startmenyn: ") 
 
 
-            if film_search == "":
-                
+            if film_search == "":                
                 print("\n Återgår till startmeny")
-
                 break
             
 
-            found = False
-            
-            for film in film_register:
-                                
+            found = False            
+            for film in film_register:                                
                 if film_search.lower() == film.titel.lower():
-                    
-                    found = True
-                    
+                    found = True                    
                     break
                 
             
             if found:
-                film_modul.film_found(film_search)
-                
-                            
+                film_modul.film_found(film_search)                            
             else:
                 film_modul.film_not_found(film_search)
 
 
-##------Menyva4 ------------------------------------------------------------------------------------------------------------------------------------
+    # Menyva 4: Ta bort
     elif meny_val == 4:
-
         while True:        
-            film_search = input("\nAnge vilken film du vill ta bort eller tryck 'Enter' för att återgå till startmenyn: ")
+            film_search = input(
+                "\nAnge vilken film du vill ta bort eller "
+                "tryck 'Enter' för att återgå till startmenyn: ")
 
-            if film_search == "":
-                
+
+            if film_search == "":                
                 print("\n Återgår till startmeny")
-
                 break
             
 
             existing_film = False
-
-
-            for film in film_register:
-                
-                if film_search.lower() == film.titel.lower():
-                    
+            for film in film_register:                
+                if film_search.lower() == film.titel.lower():                    
                     existing_film = film
-
                     break
 
             
-            if existing_film:
-                
-                film_register.remove(existing_film)
-                
-                print(f"\nDu har valt att ta bort: {film_search}")            
-                
-                
+            if existing_film:                
+                film_register.remove(existing_film)                
+                print(f"\nDu har valt att ta bort: {film_search}")                            
             else:
                 film_modul.film_not_found(film_search)
 
         
-##------Menyval 5------------------------------------------------------------------------------------------------------------------------------------
-    elif meny_val == 5:
-        
-        print("\nDu lämnar nu filmregistret. Hejdå!")
-        
+    # Menyval 5: Avsluta
+    elif meny_val == 5:       
+        print("\nDu lämnar nu filmregistret. Hejdå!")        
         break
         
 
     else:
         film_modul.wrong_input()
 
-
-
-
-
-
-
-
-    
